6c5ed594419ddfc72c9987e8840df0e6c4509b9f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-07 17:12:16 +0700

    feat: add home

--- app/src/main/java/com/giftech/githubku/ui/home/HomeActivity.kt
+++ app/src/main/java/com/giftech/githubku/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.giftech.githubku
+package com.giftech.githubku.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.giftech.githubku.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
