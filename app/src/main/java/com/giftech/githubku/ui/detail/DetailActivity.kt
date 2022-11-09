4add891c9cca21e2bd64196dca75c377932a34f9
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-09 20:03:24 +0700

    Show detail user

---
+++ app/src/main/java/com/giftech/githubku/ui/detail/DetailActivity.kt
@@ -0,0 +1,64 @@
+package com.giftech.githubku.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.giftech.githubku.data.model.User
+import com.giftech.githubku.databinding.ActivityDetailBinding
+import com.giftech.githubku.ui.adapter.RepoAdapter
+import com.giftech.githubku.utils.AppUtils.loadImage
+import com.giftech.githubku.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityDetailBinding
+    private val viewModel:DetailViewModel by viewModels()
+    private lateinit var repoAdapter: RepoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupAdapter()
+        getUsername()
+        getDetailUser()
+    }
+
+    private fun getDetailUser() {
+        viewModel.user.observe(this){
+            when(it){
+                is Resource.Error -> Log.d("TAG", "getDetailUser: ${it.error}")
+                Resource.Loading -> Log.d("TAG", "getDetailUser: Loading")
+                is Resource.Success -> populateUserDetail(it.data)
+            }
+        }
+    }
+
+    private fun populateUserDetail(user: User) {
+        with(binding){
+            tvName.text = user.name
+            tvUsername.text = "@${user.username}"
+            tvBio.text = user.bio
+            ivAvatar.loadImage(user.avatar)
+        }
+    }
+
+    private fun getUsername() {
+        if(intent.extras!=null){
+            val extras = intent.extras
+            val username = extras!!.getString(USERNAME)
+            viewModel.setUsername(username!!)
+        }
+    }
+
+    private fun setupAdapter() {
+        repoAdapter = RepoAdapter()
+        binding.rvRepo.adapter = repoAdapter
+    }
+
+    companion object{
+        const val USERNAME = "USERNAME"
+    }
+}
---
+++ app/src/main/java/com/giftech/githubku/ui/detail/DetailViewModel.kt
@@ -0,0 +1,30 @@
+package com.giftech.githubku.ui.detail
+
+import androidx.lifecycle.*
+import com.giftech.githubku.data.MainRepository
+import com.giftech.githubku.data.model.User
+import com.giftech.githubku.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel
+@Inject constructor(
+    repository:MainRepository
+) : ViewModel() {
+
+    val test = "shit"
+    private val _username = MutableLiveData<String>()
+
+    private val _user = _username.switchMap { username->
+        repository.getDetailUser(username).asLiveData()
+    }
+    val user:LiveData<Resource<User>> = _user
+
+    fun setUsername(username:String){
+        if (username == _username.value || username.isEmpty()){
+            return
+        }
+        _username.value = username
+    }
+}
