e39fa0e60d68a5a5bb47a2e84ca7491ddab9d97e
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-09 16:17:33 +0700

    Store access token on local prop

--- app/src/main/java/com/giftech/githubku/utils/Constants.kt
+++ app/src/main/java/com/giftech/githubku/utils/Constants.kt
@@ -1,5 +1,8 @@
 package com.giftech.githubku.utils
 
+import com.giftech.githubku.BuildConfig
+
 object Constants {
     const val BASE_URL = "https://api.github.com"
+    const val ACCESS_TOKEN = BuildConfig.ACCESS_TOKEN
 }
