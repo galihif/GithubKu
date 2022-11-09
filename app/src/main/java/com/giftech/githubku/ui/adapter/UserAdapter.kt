3af4a442255e7b219d59e31b6a6626f5da39d815
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-09 16:05:05 +0700

    Create user adapter

---
+++ app/src/main/java/com/giftech/githubku/ui/adapter/UserAdapter.kt
@@ -0,0 +1,47 @@
+package com.giftech.githubku.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.giftech.githubku.data.model.User
+import com.giftech.githubku.databinding.ItemUserBinding
+import com.giftech.githubku.utils.AppUtils.loadImage
+
+class UserAdapter(
+    private val onClickItem:(User) -> Unit
+): ListAdapter<User, UserAdapter.UserViewHolder>(DIFF_CALLBACK) {
+
+    inner class UserViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: User){
+            binding.tvName.text = item.username
+            binding.ivAvatar.loadImage(item.avatar)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding =
+            ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val item = getItem(position)
+        if(item!=null){
+            holder.bind(item)
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
+    }
+}
