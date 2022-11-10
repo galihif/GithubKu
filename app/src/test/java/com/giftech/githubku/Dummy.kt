ed303ae65db163f4a4cb8b0b2f4b2429de53078c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-10 09:07:43 +0700

    create unit test for viewmodel

---
+++ app/src/test/java/com/giftech/githubku/Dummy.kt
@@ -0,0 +1,30 @@
+package com.giftech.githubku
+
+import com.giftech.githubku.data.model.User
+
+object Dummy {
+    fun dummyUser() =
+        listOf(
+            User(
+                id = 0,
+                username = "user",
+                avatar = ""
+            ),
+            User(
+                id = 0,
+                username = "user",
+                avatar = ""
+            ),
+            User(
+                id = 0,
+                username = "user",
+                avatar = ""
+            ),
+            User(
+                id = 0,
+                username = "user",
+                avatar = ""
+            ),
+
+        )
+}
---
+++ app/src/test/java/com/giftech/githubku/HomeViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.giftech.githubku
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import com.giftech.githubku.data.MainRepository
+import com.giftech.githubku.data.model.User
+import com.giftech.githubku.ui.home.HomeViewModel
+import com.giftech.githubku.utils.Resource
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class HomeViewModelTest {
+
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var repository: MainRepository
+
+    @Mock
+    private lateinit var viewModel: HomeViewModel
+
+    @Test
+    fun getUser(){
+        val dummyData = Resource.Success(Dummy.dummyUser())
+        val expectedUsers = MutableLiveData<Resource<List<User>>>()
+        expectedUsers.value = dummyData
+        `when`(viewModel.users).thenReturn(expectedUsers)
+
+        val actualUsers = viewModel.users.getOrAwaitValue()
+
+        verify(viewModel).users
+        assertNotNull(actualUsers)
+
+    }
+
+}
